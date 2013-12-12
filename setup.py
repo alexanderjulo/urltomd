6f28ba3ec98eb4ca1239f011dae8773eeeb11e7f
Alexander Jung-Loddenkemper <alexander@julo.ch>
2013-12-12 10:57:06 +0100

    prepare urltomd for packaging.

---
+++ setup.py
@@ -0,0 +1,32 @@
+"""
+    urltomd
+    ----------------
+
+    A library to map url structures onto files with yaml metadata and a
+    markdown body. Perfect for static file cmses, blogs and alike.
+
+    Links
+    `````
+
+    * `source code <https://github.com/alexex/urltomd>`_
+"""
+from setuptools import setup
+
+
+setup(
+    name='urltomd',
+    version='0.1',
+    url='https://github.com/alexex/urltomd',
+    license='MIT',
+    author='Alexander Jung-Loddenkemper',
+    author_email='alexander@julo.ch',
+    description='A library for mapping urls on to markdown files with yaml metadata',
+    long_description=__doc__,
+    packages=['urltomd'],
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'pyyaml',
+        'misaka'
+    ],
+)
